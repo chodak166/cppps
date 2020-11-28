1d931d6f61a0f619359ce86830a436af9ee60262
chodak166 <chodak166@op.pl>
2020-11-28 18:39:32 +0100

    Added dummy unit tests

---
+++ tests/unit/GraphDependencyResolver.test.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2020  Lukasz Chodyla
+// Distributed under the Boost Software License v1.0.
+// See accompanying file LICENSE.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt for the full license.
+
+
+#include <catch2/catch.hpp>
+//#include <fakeit/catch/fakeit.hpp>
+
+#include <nixlab/stdeasylog> //TODO: remove
+
+//NOTE: use -Wno-disabled-macro-expansion to suppress fakeit macro warnings
+//using namespace fakeit;
+
+namespace {
+
+}
+
+
+namespace test {
+namespace {
+
+
+} // namespace
+} // namespace test
+
+
+TEST_CASE("Testing graph dependency resolver", "[gdr]")
+{
+
+
+}
+
+
+namespace test {
+namespace {
+
+
+} // namespace
+} // namespace test
+
