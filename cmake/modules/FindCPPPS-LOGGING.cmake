17f849ec809ca1d9f71df79e476cf8e4281db043
chodak166 <chodak166@op.pl>
2022-11-14 17:29:22 +0100

    Restored shared cmake targets export

--- cmake/modules/FindCPPPS-LOGGING.cmake
+++ cmake/modules/FindCPPPS-LOGGING.cmake
@@ -1,3 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cppps_module_helper.cmake)
 CPPPS_ADD_MODULE("logging" cppps-logging-static cppps::logging)
-#CPPPS_ADD_MODULE("logging" cppps-logging-shared cppps::logging-shared)
+CPPPS_ADD_MODULE("logging" cppps-logging-shared cppps::logging-shared)
